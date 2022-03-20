9accf97f4413ab4a54575b6dac8a205e8c00d28a
Andrey1771 <Hohlov908@gmail.com>
2022-03-20 22:49:30 +0300

    completed JSONSaveLoader

--- JSONSaveLoaderLibrary/JSONSaveLoader.cs
+++ JSONSaveLoaderLibrary/JSONSaveLoader.cs
@@ -3,38 +3,65 @@ using System.Collections.Generic;
 using System.Text;
 using System.IO;
 using TranslatorToolsLibrary.DI;
+using System.Threading.Tasks;
+using System.Threading;
 
 namespace JSONSaveLoaderLibrary
 {
-    public class JSONSaveLoader<T> : IFileController<T>
+    public class JSONSaveLoader<T> : IFileController<T>, IAsyncFileController<T>
     {
-        public ICollection<T> Load(string path)
-        {
-            return Deserialize(path);
-        }
-
         public void Save(ICollection<T> data, string path)
         {
             Serialize(data, path);
         }
 
+        public ICollection<T> Load(string path)
+        {
+            return Deserialize(path);
+        }
+
+        public Task SaveAsync(ICollection<T> data, string path)
+        {
+            return Task.Run(() =>
+            {
+                Serialize(data, path);
+            });
+        }
+
+        public Task<ICollection<T>> LoadAsync(string path)
+        {
+
+            return Task.Run(() =>
+            {
+                return Deserialize(path);
+            });
+        }
+
         private void Serialize(ICollection<T> data, string path)
         {
-            Newtonsoft.Json.JsonSerializer serializer = new Newtonsoft.Json.JsonSerializer();
-            serializer.NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore;
-            serializer.TypeNameHandling = Newtonsoft.Json.TypeNameHandling.Auto;
-            serializer.Formatting = Newtonsoft.Json.Formatting.Indented;
-
-            using (StreamWriter sw = new StreamWriter(path))
-            using (Newtonsoft.Json.JsonWriter writer = new Newtonsoft.Json.JsonTextWriter(sw))
+            try
             {
-                serializer.Serialize(writer, data, typeof(ICollection<T>));
+                Newtonsoft.Json.JsonSerializer serializer = new Newtonsoft.Json.JsonSerializer();
+                serializer.NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore;
+                serializer.TypeNameHandling = Newtonsoft.Json.TypeNameHandling.Auto;
+                serializer.Formatting = Newtonsoft.Json.Formatting.Indented;
+
+                using (StreamWriter sw = new StreamWriter(path))
+                using (Newtonsoft.Json.JsonWriter writer = new Newtonsoft.Json.JsonTextWriter(sw))
+                {
+                    serializer.Serialize(writer, data, typeof(ICollection<T>));
+                }
+            }
+            catch (Exception ex)
+            {
+                // Todo add logger
+                throw ex;
             }
         }
 
         private ICollection<T> Deserialize(string path)
         {
-            if (File.Exists(path))
+            try
             {
                 var settings = new Newtonsoft.Json.JsonSerializerSettings
                 {
@@ -43,9 +70,11 @@ namespace JSONSaveLoaderLibrary
                 };
                 return Newtonsoft.Json.JsonConvert.DeserializeObject<ICollection<T>>(File.ReadAllText(path), settings);
             }
-            return new List<T>();//TODO fix that
+            catch (Exception ex)
+            {
+                // Todo add logger
+                throw ex;
+            }
         }
-
-
     }
 }
---
+++ TranslateLibrary.DI/IAsyncFileController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TranslatorToolsLibrary.DI
+{
+    public interface IAsyncFileController<T>
+    {
+        Task SaveAsync(ICollection<T> data, string path);
+        Task<ICollection<T>> LoadAsync(string path);
+    }
+}
